ef00b5f62b99e78a7fd7b8969b92e13a3e727905
itanne99 <idovader@gmail.com>
2026-05-01 00:31:37 +0000

    feat(ui): foundation setup for Neon Court design system
    - Replaced bootswatch cyborg with standard bootstrap
    - Added Lexend font
    - Added Neon Court CSS variables and utility classes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/styles/globals.css";
+import { Lexend } from "next/font/google";
+
+const lexend = Lexend({ subsets: ['latin'], variable: '--font-lexend' });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={lexend.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
